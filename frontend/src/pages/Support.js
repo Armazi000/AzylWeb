71e73cf0cdd13efbafa68aee35b7214282d8ef15
Armazi000 <oliwierthomalla4@gmail.com>
2026-02-19 23:40:47 +0100

    Switch to background-image for slider overlay - more reliable rendering

--- frontend/src/pages/Support.js
+++ frontend/src/pages/Support.js
@@ -117,37 +117,33 @@ export default function Support() {
               {/* Main Slider Container */}
               <div
                 ref={containerRef}
-                className="relative w-full bg-gray-200 rounded-lg overflow-hidden shadow-lg cursor-col-resize select-none"
-                style={{ height: '400px', maxHeight: '500px' }}
+                className="relative w-full rounded-lg overflow-hidden shadow-lg cursor-col-resize select-none"
+                style={{
+                  height: '400px',
+                  maxHeight: '500px',
+                  backgroundImage: `url('${beforePhotos[currentIndex]}')`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center'
+                }}
                 onMouseDown={handleMouseDown}
                 onTouchStart={handleMouseDown}
                 onTouchMove={handleSliderChange}
                 onTouchEnd={handleMouseUp}
               >
                 {/* Before Image (Background) */}
-                <img
-                  src={beforePhotos[currentIndex]}
-                  alt="Przed"
-                  className="absolute inset-0 w-full h-full object-cover"
-                  onError={(e) => {
-                    e.target.src = 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 300"><rect fill="%23e0e0e0" width="400" height="300"/><text x="50%" y="50%" text-anchor="middle" dy=".3em" fill="%23999" font-size="18">Brak zdjęcia</text></svg>';
-                  }}
-                />
+                {/* Moved to container background-image above */}
 
                 {/* After Image (Slider Overlay) */}
                 <div
                   className="absolute top-0 left-0 h-full overflow-hidden"
-                  style={{ width: `${sliderPosition}%` }}
+                  style={{
+                    width: `${sliderPosition}%`,
+                    zIndex: 2,
+                    backgroundImage: `url('${afterPhotos[currentIndex]}')`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center'
+                  }}
                 >
-                  <img
-                    src={afterPhotos[currentIndex]}
-                    alt="Po"
-                    className="absolute top-0 left-0 h-full object-cover"
-                    style={{ width: '3000px' }}
-                    onError={(e) => {
-                      e.target.src = 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 300"><rect fill="%23e0e0e0" width="400" height="300"/><text x="50%" y="50%" text-anchor="middle" dy=".3em" fill="%23999" font-size="18">Brak zdjęcia</text></svg>';
-                    }}
-                  />
                 </div>
 
                 {/* Slider Handle */}
